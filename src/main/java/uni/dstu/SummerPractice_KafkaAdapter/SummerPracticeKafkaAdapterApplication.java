900c1686be5bd3d0a09c93aaeb8195cabe213feb
Andrew Pavin <2003and@gmail.com>
2024-06-10 13:29:22 +0300

    Inital commit

---
+++ src/main/java/uni/dstu/SummerPractice_KafkaAdapter/SummerPracticeKafkaAdapterApplication.java
@@ -0,0 +1,13 @@
+package uni.dstu.SummerPractice_KafkaAdapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerPracticeKafkaAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerPracticeKafkaAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/dstu/SummerPractice_KafkaAdapter/SummerPracticeKafkaAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.dstu.SummerPractice_KafkaAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerPracticeKafkaAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
